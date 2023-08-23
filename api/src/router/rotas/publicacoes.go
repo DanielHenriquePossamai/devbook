12b2bc2b37ffa1c5c0de1761ae79b4b99c4b182f
Daniel Henrique Possamai <danielh.possamai@gmail.com>
2023-08-23 17:14:42 -0300

    Finalização da API
    Feito a finalização das rotas e métodos da API.
    
    Agora ficou somente algumas situações para verificar e algumas rotas para validar.

---
+++ api/src/router/rotas/publicacoes.go
@@ -0,0 +1,57 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasPublicacoes = []Rota{
+	{
+		URI:                "/publicacoes",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacoes,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoid}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoid}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizaPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoid}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}/publicacoes",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacoesPorUsuario,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoid}/curtir",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CurtirPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoid}/descurtir",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.DescurtirPublicacao,
+		RequerAutenticacao: true,
+	},
+}
